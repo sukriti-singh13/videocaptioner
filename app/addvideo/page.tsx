5b850153ca184423d133a914270bdefae092958f
Sukriti Singh <sukriti.singh899@gmail.com>
2024-05-22 21:50:07 +0530

    adds check for adding video url

--- app/addvideo/page.tsx
+++ app/addvideo/page.tsx
@@ -1,26 +1,46 @@
 'use client';
+import Toast from '@/components/Toast/Toast';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
 const Page = () => {
+    const regexForValidUrl =/^https?:\/\/(?:www\.)?[\w\-]+(\.[\w\-]+)+\/[\w\-\/\.]+\.mp4\b/
   const [videoUrl, setVideoUrl] = useState('');
+  const [toast, setToast] = useState({ message: '', type: '' });
   const router = useRouter();
   const onVideoUrlSubmit = () => {
-    if (videoUrl === '') return;
+    if (videoUrl === '' || !regexForValidUrl.test(videoUrl) ){
+      setToast({ message: 'Please enter a valid URL', type: 'error' });
+      let toastTime = setTimeout(() => {
+        setToast({ message: '', type: '' });
+      }, 3000);
+      return () => clearTimeout(toastTime);
+    }
     router.push(`/editor?videoUrl=${videoUrl}`);
     console.log('Video Url Submitted');
   };
   return (
-    <div className='h-screen flex flex-col bg-slate-900'>
-      <h1 className='text-center text-white font-medium text-2xl pt-10'>Video Captioner</h1>
-
-     <div className='flex flex-col gap-10 justify-center items-center flex-1 h-full'> <input
-        placeholder='Add Video Url'
-        value={videoUrl}
-        className='w-[40%] py-2 px-4 rounded-sm text-black'
-        onChange={(e) => setVideoUrl(e.target.value)}
-      />
-      <button  className='bg-blue-500 text-white rounded-md py-2 px-6' onClick={onVideoUrlSubmit}>Submit</button></div>
+    <div className='h-screen flex flex-col bg-slate-900 relative'>
+        <Toast message={toast.message} type={toast.type}/>
+      <h1 className='text-center text-white font-medium text-2xl pt-10'>
+        Video Captioner
+      </h1>
+      
+      <div className='flex flex-col gap-10 justify-center items-center flex-1 h-full'>
+        {' '}
+        <input
+          placeholder='Add Video Url'
+          value={videoUrl}
+          className='w-[40%] py-2 px-4 rounded-sm text-black'
+          onChange={(e) => setVideoUrl(e.target.value)}
+        />
+        <button
+          className='bg-blue-500 text-white rounded-md py-2 px-6'
+          onClick={onVideoUrlSubmit}
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -1,8 +1,9 @@
-import './Toast.scss';
+
 import { TToast } from './Toast.types';
 const Toast = ({ message, type }: TToast) => {
+  if(message === '') return null;
   return (
-    <div className={`toast ${type === 'success' ? 'success' : 'error'}`}>
+    <div className={`absolute bottom-10 right-10 text-white rounded-md px-3 text-sm py-2 ${type === 'success' ? 'bg-[#a6d96a]' : 'bg-[#d7191c]'}`}>
       {message}
     </div>
   );
